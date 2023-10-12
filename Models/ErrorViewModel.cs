24b0756a9ace9ae9888e5f26f429383b8336aaac
MATIAS IVAN SZABO <47805194@ort>
2023-10-12 13:28:52 -0300

    create

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tp10_Mi_sitio_de_Series.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
